4d97aaf5240fc15758e4750ad8397c7519c960ee
Jay <jeremyt89@ymail.com>
2024-06-17 12:34:03 -0400

    put method

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,6 +14,15 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
+  const db = await openDB('jate', 1);
+const tx = db.transaction('jate', 'readwrite');
+const store = tx.objectStore('jate');
+const request = store.put({id:1, value:content});
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('result.value', result.value);
+  
 
 }
 
